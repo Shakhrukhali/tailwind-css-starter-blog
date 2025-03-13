8b136236f6a13032b88ed8591c989553141fea13
ShoxruxC <shakhrukhali@gmail.com>
2025-03-13 14:33:18 +0500

    update

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,8 +1,8 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js test test Blog',
+  title: 'SwiftUI || Blog',
   author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
+  headerTitle: 'SwiftUi',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
