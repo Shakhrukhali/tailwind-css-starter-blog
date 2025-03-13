5e9610f7764036f9193e9c9403b0a83ef0f9b259
ShoxruxC <shakhrukhali@gmail.com>
2025-03-13 16:58:43 +0500

    aa

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -28,7 +28,6 @@ export default function Footer() {
         </div>
         <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
           <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
-            Test
           </Link>
         </div>
       </div>
